612c699d87640ac35881d4107dcab9f629f379e1
nouman <nouman_razzaq>
2020-11-26 01:48:44 +0500

    add icon and text

--- lib/main.dart
+++ lib/main.dart
@@ -11,82 +11,242 @@ class BMICalculator extends StatelessWidget {
         primaryColor: Color(0xFF0A0E1),
         scaffoldBackgroundColor: Color(0xF0A0E21),
       ),
-      home: InputPage(How to reimport 'package:flutter/material.dart';
+      home: InputPage(import 'package:flutter/material.dart';
+      import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+      import 'package:bmi_calculator/components/icon_content.dart';
+      import 'package:bmi_calculator/components/reusable_card.dart';
       import 'package:bmi_calculator/constants.dart';
-          import 'package:bmi_calculator/components/reusable_card.dart';
-          import 'package:bmi_calculator/components/bottom_button.dart';
+      import 'package:bmi_calculator/screens/results_page.dart';
+      import 'package:bmi_calculator/components/bottom_button.dart';
+      import 'package:bmi_calculator/components/round_icon_button.dart';
+      import 'package:bmi_calculator/calculator_brain.dart';
 
-          class ResultsPage extends StatelessWidget {
-          ResultsPage(
-          {@required this.interpretation,
-          @required this.bmiResult,
-          @required this.resultText});
+      enum Gender {
+      male,
+      female,
+      }
 
-          final String bmiResult;
-          final String resultText;
-          final String interpretation;
+      class InputPage extends StatefulWidget {
+      @override
+      _InputPageState createState() => _InputPageState();
+      }
 
-          @override
-          Widget build(BuildContext context) {
-          return Scaffold(
-          appBar: AppBar(
-          title: Text('BMI CALCULATOR'),
-          ),
-          body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-          Expanded(
-          child: Container(
-          padding: EdgeInsets.all(15.0),
-          alignment: Alignment.bottomLeft,
-          child: Text(
-          'Your Result',
-          style: kTitleTextStyle,
-          ),
-          ),
-          ),
-          Expanded(
-          flex: 5,
-          child: ReusableCard(
-          colour: kActiveCardColour,
-          cardChild: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-          Text(
-          resultText.toUpperCase(),
-          style: kResultTextStyle,
-          ),
-          Text(
-          bmiResult,
-          style: kBMITextStyle,
-          ),
-          Text(
-          interpretation,
-          textAlign: TextAlign.center,
-          style: kBodyTextStyle,
-          ),
-          ],
-          ),
-          ),
-          ),
-          BottomButton(
-          buttonTitle: 'Re CALCULAWrite code for Input Page in ScreenTE',
-          onTap: () {
-          Navigator.pop(context);
-          },
-          )
-          ],
-          ),
-          );
-          }
+      class _InputPageState extends State<InputPage> {
+      Gender selectedGender;
+      int height = 180;
+      int weight = 60;
+      int age = 20;
 
+      @override
+      Widget build(BuildContext context) {
+      return Scaffold(
+      appBar: AppBar(
+      title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+      Expanded(
+      child: Row(
+      children: <Widget>[
+      Expanded(
+      child: ReusableCard(
+      onPress: () {
+      setState(() {
+      selectedGender = Gender.male;
+      });
+      },
+      colour: selectedGender == Gender.male
+      ? kActiveCardColour
+          : kInactiveCardColour,
+      cardChild: IconContent(
+      icon: FontAwesomeIcons.mars,
+      label: 'Male',
+      ),
+      ),
+      ),
+      Expanded(
+      child: ReusableCard(
+      onPress: () {
+      setState(() {
+      selectedGender = Gender.female;
+      });
+      },
+      colour: selectedGender == Gender.female
+      ? kActiveCardColour
+          : kInactiveCardColour,
+      cardChild: IconContent(
+      icon: FontAwesomeIcons.venus,
+      label: 'Female',
+      ),
+      ),
+      ),
+      ],
+      )),
+      Expanded(
+      child: ReusableCard(
+      colour: kActiveCardColour,
+      cardChild: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+      Text(
+      'HEIGHT',
+      style: kLabelTextStyle,
+      ),
+      Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      textBaseline: TextBaseline.alphabetic,
+      children: <Widget>[
+      Text(
+      height.toString(),
+      style: kNumberTextStyle,
+      ),
+      Text(
+      'cm',
+      style: kLabelTextStyle,
+      )
+      ],
+      ),
+      SliderTheme(
+      data: SliderTheme.of(context).copyWith(
+      inactiveTrackColor: Color(0xFF8D8E98),
+      activeTrackColor: Colors.white,
+      thumbColor: Color(0xFFEB1555),
+      overlayColor: Color(0x29EB1555),
+      thumbShape:
+      RoundSliderThumbShape(enabledThumbRadius: 15.0),
+      overlayShape:
+      RoundSliderOverlayShape(overlayRadius: 30.0),
+      ),
+      child: Slider(
+      value: height.toDouble(),
+      min: 120.0,
+      max: 220.0,
+      onChanged: (double newValue) {
+      setState(() {
+      height = newValue.round();
+      });
+      },
+      ),
+      ),
+      ],
+      ),
+      ),
+      ),
+      Expanded(
+      child: Row(
+      children: <Widget>[
+      Expanded(
+      child: ReusableCard(
+      colour: kActiveCardColour,
+      cardChild: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+      Text(
+      'WEIGHT',
+      style: kLabelTextStyle,
+      ),
+      Text(
+      weight.toString(),
+      style: kNumberTextStyle,
+      ),
+      Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+      RoundIconButton(
+      icon: FontAwesomeIcons.minus,
+      onPressed: () {
+      setState(() {
+      weight--;
+      });
+      }),
+      SizedBox(
+      width: 10.0,
+      ),
+      RoundIconButton(
+      icon: FontAwesomeIcons.plus,
+      onPressed: () {
+      setState(() {
+      weight++;
+      });
+      },
+      ),
+      ],
+      ),
+      ],
+      ),
+      ),
+      ),
+      Expanded(
+      child: ReusableCard(
+      colour: kActiveCardColour,
+      cardChild: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+      Text(
+      'AGE',
+      style: kLabelTextStyle,
+      ),
+      Text(
+      age.toString(),
+      style: kNumberTextStyle,
+      ),
+      Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+      RoundIconButton(
+      icon: FontAwesomeIcons.minus,
+      onPressed: () {
+      setState(
+      () {
+      age--;
+      },
+      );
+      },
+      ),
+      SizedBox(
+      width: 10.0,
+      ),
+      RoundIconButton(
+      icon: FontAwesomeIcons.plus,
+      onPressed: () {
+      setState(() {
+      age++;
+      });
+      })
+      ],
+      )
+      ],
+      ),
+      ),
+      ),
+      ],
+      ),
+      ),
+      BottomButton(
+      buttonTitle: 'CALCULATE',
+      onTap: () {
+      CalculatorBrain calc =
+      CalculatorBrain(height: height, weight: weight);
 
-          Leave a comment
-          No file chosen
-          Attach files by dragging & dropping, selecting or pasting them.
-          }factor our code in flutter ),
+      Navigator.push(
+      context,
+      MaterialPageRoute(
+      builder: (context) => ResultsPage(
+      bmiResult: calc.calculateBMI(),
+      resultText: calc.getResult(),
+      interpretation: calc.getInterpretation(),
+      ),
+      ),
+      );
+      },
+      ),
+      ],
+      ),
+      );
+      }
+      } ),
     );
   }
 }
