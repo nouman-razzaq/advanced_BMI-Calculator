a934f3768159287503a9246c1567ed94a16dd3ae
nouman <nouman_razzaq>
2020-11-26 01:38:27 +0500

    Add Library in pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext copyWitcontext) {
     return MaterialApp(
       theme: ThemeData.dark().h(
         primaryColor: Color(0xFF0A0E1),
